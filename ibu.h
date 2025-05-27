db239a7a9119b47652e87d78405a14ec94503c21
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-05-27 10:18:27 +0700

    menambahkan deklarasi fungsi anggota tambahAnak dan cetakAnak

--- ibu.h
+++ ibu.h
@@ -9,4 +9,9 @@ Class ibu{
           ibu(string pNama): nama(pNama){
             cout<< "Ibu\""<<nama<<"\" ada\n";
           }
-} 
+          ~ibu(){
+            cout <<"Ibu \""<<nama<< "\" tidak ada\n";
+            void tambahAnak(anak*);
+            void cetakAnak();
+          }
+};
